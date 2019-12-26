728a7797cd0564c2f47e2be48403123cc31dff59
WANNA <2457684290@qq.com>
2019-12-26 11:21:58 +0800

    'errorImg也使用liadingImg'

--- src/main.js
+++ src/main.js
@@ -2,13 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueLazyLoad from 'vue-lazyload'
 import loadingImg from './assets/img/loading.jpg'
-import errorImg from './assets/img/error.jpg'
 
 Vue.config.productionTip = false
 
 Vue.use(VueLazyLoad, {
   preload: 1.3, // 预加载高度比例
-  error: errorImg, // 图片路径出错时加载图片
+  error: loadingImg, // 图片路径出错时加载图片
   loading: loadingImg, // 预加载图片
   attempt: 6, // 尝试加载图片数量
   observer: true,
