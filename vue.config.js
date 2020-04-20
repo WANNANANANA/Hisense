d158543fac5dd18419aafd2eef9a3f26316c2247
WANNA <2457684290@qq.com>
2020-04-20 17:39:13 +0800

    '相对地址'

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 module.exports = {
-    outputDir: 'dist',
-    publicPath: 'http://www.huizhangongsi.com/hisensePro/',
+    publicPath: './',
     devServer: {
         port: 8888,
         open: true
