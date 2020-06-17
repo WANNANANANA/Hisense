de9d64267b2c899ec6b0b5f4ca8ba560771f372d
WANNA <2457684290@qq.com>
2020-06-17 18:02:50 +0800

    '添加gizp优化项'

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,40 @@
-const path = require('path');
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+
 module.exports = {
     publicPath: './',
     devServer: {
         port: 8888,
         open: true
+    },
+    configureWebpack: config => {
+        const plugins = [];
+        if (process.env.NODE_ENV === 'production') {
+            // gzip压缩
+            plugins.push(
+                new CompressionWebpackPlugin({
+                    filename: '[path].gz[query]',
+                    algorithm: 'gzip',
+                    test: new RegExp('\\.(js|css)$'),
+                    threshold: 10240,
+                    minRatio: 0.8
+                })
+            )
+            config.externals = {
+                'vue': 'Vue'
+            }
+        }
+        config.plugins = [
+            ...config.plugins,
+            ...plugins
+        ]
+    },
+    chainWebpack: config => {
+        if (process.env.NODE_ENV === 'production') {
+            config.plugin('html')
+                .tap(args => {
+                    args[0].cdn = 'prod';
+                    return args;
+                });
+        }
     }
 }
